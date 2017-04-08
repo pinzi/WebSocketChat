66823281b34bf244c4676c9a5e9ef51d69a06193
pinzi <lp900219@163.com>
2017-04-08 19:13:33 +0800

    基本框架搭建

---
+++ Server/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Server.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public JsonResult Reg()
+        {
+            return View();
+        }
+    }
+}
